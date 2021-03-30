1685748f164f5284e3f445d6b7ddf3b4f1879b58
ergeshmametuulukanat <ergeshmametuulukanat@gmail.com>
2021-03-30 22:58:07 +0600

    deleted target

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -5,10 +5,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Hello World:" + (bean == bean2));
         System.out.println(bean.getMessage());
+
+        Cat cat = applicationContext.getBean("cat",Cat.class);
+        Cat cat2 = applicationContext.getBean("cat",Cat.class);
+        System.out.println("Cat = Cat2 ?" + (cat == cat2));
+        System.out.println(cat);
+
     }
 }
---
+++ src/main/java/peaksoft/Cat.java
@@ -0,0 +1,15 @@
+package peaksoft;
+
+public class Cat {
+    private String cat;
+
+    public Cat() {
+    }
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+}
